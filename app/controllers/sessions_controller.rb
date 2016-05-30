211ffc16a4ea4f9c41553bf26c480e0640ef230d
Suyesh Bhandari <suyeshb@gmail.com>
2016-05-30 01:39:20 -0400

    .

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def login
     @user = User.find(params[:id])
     session[:guest_user_id] = @user.id
-    @todos = @user.todos.all
-    @todo = @user.todos.new
+    redirect_to root_path
   end
 end
